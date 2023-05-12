70d5676a9460e52a03064a8efdad9b6692e6c612
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-05-12 17:40:04 +0900

    マジックナンバーを定数定義

--- lib/jan_to_isbn_string_extension.dart
+++ lib/jan_to_isbn_string_extension.dart
@@ -1,19 +1,27 @@
 extension JanToIsbnStringExtension on String {
   String convertJanToIsbn() {
-    if (length == 13) {
-      String isbn = substring(3, 12);
+    int janCodeLength = 13;
+    int startIsbnIndex = 3;
+    int endIsbnIndex = 12;
+    int checkDigitLoopCount = 9;
+    int checkDigitModulo = 11;
+    int checkDigitXValue = 10;
+
+    if (length == janCodeLength) {
+      String isbn = substring(startIsbnIndex, endIsbnIndex);
       int checkDigit = 0;
 
       try {
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < checkDigitLoopCount; i++) {
           checkDigit += int.parse(isbn[i]) * (i + 1);
         }
       } on FormatException {
         throw const FormatException('不正なJANコードです');
       }
 
-      checkDigit %= 11;
-      if (checkDigit == 10) {
+      checkDigit %= checkDigitModulo;
+
+      if (checkDigit == checkDigitXValue) {
         isbn += 'X';
       } else {
         isbn += checkDigit.toString();
