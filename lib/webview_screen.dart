b3a4298625276c1e98a420c9558a4d26dd77e485
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-05-17 17:20:46 +0900

    WebViewScreenをstatefulに

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewScreen extends StatelessWidget {
+class WebViewScreen extends StatefulWidget {
   final String url;
 
-  const WebViewScreen({super.key, required this.url});
+  const WebViewScreen({Key? key, required this.url}) : super(key: key);
 
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: WebView(
-          initialUrl: url,
+          initialUrl: widget.url,
           javascriptMode: JavascriptMode.unrestricted,
         ),
       ),
