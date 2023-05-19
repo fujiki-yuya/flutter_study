a8348ee3f566b7af7db621eb53bd964414afcabc
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-05-19 14:19:52 +0900

    検索画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:count_up_app/search_screen.dart';
+import 'package:count_up_app/seatch_repository_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SearchScreen(),
+      home: const SearchRepositoryScreen(),
     );
   }
 }
