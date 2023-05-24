1464f02dd2152672f22596ff9da05bb483026675
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-05-24 17:31:13 +0900

    analyze対応

--- lib/api/github_api.g.dart
+++ lib/api/github_api.g.dart
@@ -47,8 +47,6 @@ Map<String, dynamic> _$PullRequestToJson(PullRequest instance) =>
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
-
 class _GitHubApi implements GitHubApi {
   _GitHubApi(
     this._dio, {
@@ -63,27 +61,28 @@ class _GitHubApi implements GitHubApi {
 
   @override
   Future<List<Issue>> getIssues(
-    owner,
-    repo,
+    String owner,
+    String repo,
   ) async {
-    const _extra = <String, dynamic>{};
+    const extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
-    final _result =
-        await _dio.fetch<List<dynamic>>(_setStreamType<List<Issue>>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
+    final headers = <String, dynamic>{};
+    final result = await _dio.fetch<List<dynamic>>(
+      _setStreamType<List<Issue>>(
+        Options(
+          method: 'GET',
+          headers: headers,
+          extra: extra,
+        )
             .compose(
               _dio.options,
-              '/repos/${owner}/${repo}/issues',
+              '/repos/$owner/$repo/issues',
               queryParameters: queryParameters,
-              data: _data,
             )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    var value = _result.data!
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl),
+      ),
+    );
+    final value = result.data!
         .map((dynamic i) => Issue.fromJson(i as Map<String, dynamic>))
         .toList();
     return value;
@@ -91,27 +90,28 @@ class _GitHubApi implements GitHubApi {
 
   @override
   Future<List<Pull>> getPulls(
-    owner,
-    repo,
+    String owner,
+    String repo,
   ) async {
-    const _extra = <String, dynamic>{};
+    const extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
-    final _result =
-        await _dio.fetch<List<dynamic>>(_setStreamType<List<Pull>>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
+    final headers = <String, dynamic>{};
+    final result = await _dio.fetch<List<dynamic>>(
+      _setStreamType<List<Pull>>(
+        Options(
+          method: 'GET',
+          headers: headers,
+          extra: extra,
+        )
             .compose(
               _dio.options,
-              '/repos/${owner}/${repo}/pulls',
+              '/repos/$owner/$repo/pulls',
               queryParameters: queryParameters,
-              data: _data,
             )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    var value = _result.data!
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl),
+      ),
+    );
+    final value = result.data!
         .map((dynamic i) => Pull.fromJson(i as Map<String, dynamic>))
         .toList();
     return value;
