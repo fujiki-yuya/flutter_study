313b1d2796989a97586f4f493da252d85ab82608
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-06-01 10:58:41 +0900

    nullチェック追加

--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -26,7 +26,16 @@ class _NewsScreenState extends State<NewsScreen> {
     final dio = Dio();
     final newsApi = NewsApi(dio);
     final apiKey = dotenv.env['NEWS_API_KEY'];
-    await newsApi.getNews(apiKey!).then((response) {
+    if (apiKey == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('APIキーが設定されていません'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+      return;
+    }
+    await newsApi.getNews(apiKey).then((response) {
       setState(() {
         _news = response;
       });
@@ -71,7 +80,7 @@ class _NewsScreenState extends State<NewsScreen> {
                     final title = _news?.articles?[index].title;
                     return ListTile(
                       title: Text(
-                        title!,
+                        title ?? 'ニュースがありません',
                       ),
                       trailing: GestureDetector(
                         onTapDown: (TapDownDetails details) {
@@ -80,27 +89,30 @@ class _NewsScreenState extends State<NewsScreen> {
                         child: const Icon(Icons.favorite),
                       ),
                       onTap: () async {
-                        await Navigator.push(
-                          context,
-                          MaterialPageRoute<Widget>(
-                            builder: (context) => NewsWebView(
-                              url: _news!.articles![index].url!,
-                            ),
-                          ),
-                        ).catchError((dynamic e) {
-                          return AlertDialog(
-                            title: const Text('ニュースを表示できません'),
-                            content: Text(e.toString()),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('閉じる'),
+                        final url = _news?.articles?[index].url;
+                        if (url != null) {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute<Widget>(
+                              builder: (context) => NewsWebView(
+                                url: url,
                               ),
-                            ],
-                          );
-                        });
+                            ),
+                          ).catchError((dynamic e) {
+                            return AlertDialog(
+                              title: const Text('ニュースを表示できません'),
+                              content: Text(e.toString()),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('閉じる'),
+                                ),
+                              ],
+                            );
+                          });
+                        }
                       },
                     );
                   },
