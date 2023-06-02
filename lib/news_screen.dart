8911e8c9ad726e2b3d07fc1e86b64a485b06e8ab
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-06-02 10:14:58 +0900

    ニュース更新機能実装

--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -14,17 +14,19 @@ class NewsScreen extends StatefulWidget {
 }
 
 class _NewsScreenState extends State<NewsScreen> {
+  final Dio _dio = Dio();
+  late final NewsApi _newsApi;
+
   NewsResult? _news;
 
   @override
   void initState() {
     super.initState();
+    _newsApi = NewsApi(_dio);
     getNews();
   }
 
   Future<void> getNews() async {
-    final dio = Dio();
-    final newsApi = NewsApi(dio);
     final apiKey = dotenv.env['NEWS_API_KEY'];
     if (apiKey == null) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -35,7 +37,7 @@ class _NewsScreenState extends State<NewsScreen> {
       );
       return;
     }
-    await newsApi.getNews(apiKey).then((response) {
+    await _newsApi.getNews(apiKey).then((response) {
       setState(() {
         _news = response;
       });
@@ -51,75 +53,71 @@ class _NewsScreenState extends State<NewsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('ニュース一覧'),
-          leading: IconButton(
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ニュース一覧'),
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: getNews,
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () {},
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: getNews,
+        child: Column(
+          children: [
+            const SizedBox(height: 80),
+            Flexible(
+              child: ListView.builder(
+                itemCount: _news?.articles?.length ?? 0,
+                itemBuilder: (context, index) {
+                  final title = _news?.articles?[index].title;
+                  return ListTile(
+                    title: Text(
+                      title ?? 'ニュースがありません',
+                    ),
+                    trailing: GestureDetector(
+                      onTapDown: (TapDownDetails details) {
+                        // お気に入りボタンが押された時の処理
+                      },
+                      child: const Icon(Icons.favorite),
+                    ),
+                    onTap: () async {
+                      final url = _news?.articles?[index].url;
+                      if (url != null) {
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute<Widget>(
+                            builder: (context) => NewsWebView(
+                              url: url,
+                            ),
+                          ),
+                        ).catchError((dynamic e) {
+                          return AlertDialog(
+                            title: const Text('ニュースを表示できません'),
+                            content: Text(e.toString()),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('閉じる'),
+                              ),
+                            ],
+                          );
+                        });
+                      }
+                    },
+                  );
+                },
+              ),
             ),
           ],
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              const SizedBox(height: 80),
-              Flexible(
-                child: ListView.builder(
-                  itemCount: _news?.articles?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    final title = _news?.articles?[index].title;
-                    return ListTile(
-                      title: Text(
-                        title ?? 'ニュースがありません',
-                      ),
-                      trailing: GestureDetector(
-                        onTapDown: (TapDownDetails details) {
-                          // お気に入りボタンが押された時の処理
-                        },
-                        child: const Icon(Icons.favorite),
-                      ),
-                      onTap: () async {
-                        final url = _news?.articles?[index].url;
-                        if (url != null) {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute<Widget>(
-                              builder: (context) => NewsWebView(
-                                url: url,
-                              ),
-                            ),
-                          ).catchError((dynamic e) {
-                            return AlertDialog(
-                              title: const Text('ニュースを表示できません'),
-                              content: Text(e.toString()),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text('閉じる'),
-                                ),
-                              ],
-                            );
-                          });
-                        }
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
