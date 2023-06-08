6169e06c02d2f518206d2d94503fcb6366b5a26a
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-06-08 17:29:31 +0900

    コード見直し、修正

--- lib/api/news_api.g.dart
+++ lib/api/news_api.g.dart
@@ -6,8 +6,6 @@ part of 'news_api.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
-
 class _NewsApi implements NewsApi {
   _NewsApi(
     this._dio, {
@@ -21,25 +19,26 @@ class _NewsApi implements NewsApi {
   String? baseUrl;
 
   @override
-  Future<NewsResult> getNews(apiKey) async {
-    const _extra = <String, dynamic>{};
+  Future<NewsResult> getNews(String apiKey) async {
+    const extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<NewsResult>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
+    final headers = <String, dynamic>{};
+    final result = await _dio.fetch<Map<String, dynamic>>(
+      _setStreamType<NewsResult>(
+        Options(
+          method: 'GET',
+          headers: headers,
+          extra: extra,
+        )
             .compose(
               _dio.options,
-              'top-headlines?country=jp&apiKey=${apiKey}',
+              'top-headlines?country=jp&apiKey=$apiKey',
               queryParameters: queryParameters,
-              data: _data,
             )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = NewsResult.fromJson(_result.data!);
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl),
+      ),
+    );
+    final value = NewsResult.fromJson(result.data!);
     return value;
   }
 
