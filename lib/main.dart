916bc84a3e82be050169a31dd4f4b65da3126823
fujiki-yuya <fujiki_yuya@retail-ai.jp>
2023-06-22 09:35:01 +0900

    apiキー隠蔽のやり方変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:count_up_app/news_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load();
   runApp(const MyApp());
 }
 
--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -1,7 +1,6 @@
 import 'package:count_up_app/news_webview.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'api/news_api.dart';
 import 'favorite_news_screen.dart';
@@ -61,8 +60,8 @@ class _NewsScreenState extends State<NewsScreen> {
   }
 
   Future<String> _checkAPIKey() async {
-    final apiKey = dotenv.env['NEWS_API_KEY'];
-    if (apiKey == null) {
+    const apiKey = String.fromEnvironment('NEWS_API_KEY');
+    if (apiKey.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('APIキーが設定されていません'),
